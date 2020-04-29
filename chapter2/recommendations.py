4caacfd134e82d721fa8f3d366e5ec31cc240a06
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-04-29 18:15:44 +0900

    予めデータセットを用意した上で、類似スコアを計算

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -109,3 +109,36 @@ def transformPrefs(prefs):
             # itemとpersonを入れ替える
             result[item][person] = prefs[person][item]
     return result
+
+def calculateSimilarItems(prefs, n=10):
+    # アイテムをキーとして持ち、それぞれのアイテムに似ているアイテムのリストを値として持つディクショナリを作る
+    result = {}
+    itemPrefs = transformPrefs(prefs)
+
+    c = 0
+    for item in itemPrefs:
+        c += 1
+        if c % 100 == 0: print(f"{c} / {len(itemPrefs)}")
+        scores = topMatches(itemPrefs, item, n= n, similarity=sim_distance)
+        result[item] = scores
+    return result
+
+def getRecommendedItems(prefs, itemMatch, user):
+    userRatings = prefs[user]
+    scores = {}
+    totalSim = {}
+
+    for (item, rating) in userRatings.items():
+        for (similarity, item2) in itemMatch[item]:
+            if item2 in userRatings: continue
+
+            scores.setdefault(item2, 0)
+            scores[item2] += similarity * rating
+            totalSim.setdefault(item2, 0)
+            totalSim[item2] += similarity
+
+    rankings = [(score/totalSim[item], item) for item, score in scores.items()]
+
+    rankings.sort()
+    rankings.reverse()
+    return rankings
