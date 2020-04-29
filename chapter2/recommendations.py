77cc8acf8fa159e50f28e4e04e4a268c1b99619d
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-04-29 15:06:25 +0900

    ピアソン相関の実装

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -34,3 +34,31 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum(pow(prefs[person1][item]-prefs[person2][item], 2) for item in shared_items)
 
     return 1/(1+sum_of_squares)
+
+def sim_pearson(prefs, p1, p2):
+    # 二人共評価しているアイテムのリストを取得
+    shared_items={}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            shared_items[item] = 1
+
+    n = len(shared_items)
+    if n==0: return 0
+
+    # それぞれの評者の評点の合計を算出
+    sum1 = sum([prefs[p1][item] for item in shared_items])
+    sum2 = sum([prefs[p2][item] for item in shared_items])
+    # それぞれの評者の評点の平方の合計を算出
+    sum1Sqrt = sum([pow(prefs[p1][item], 2) for item in shared_items])
+    sum2Sqrt = sum([pow(prefs[p2][item], 2) for item in shared_items])
+
+    # 評価をかけ合わせた値の合計を算出
+    pSum = sum([prefs[p1][item] * prefs[p2][item] for item in shared_items])
+
+    # ピアソンによるスコアを計算
+    num = pSum - (sum1*sum2) / n
+    den = sqrt((sum1Sqrt - pow(sum1, 2) / n) * (sum2Sqrt - pow(sum2, 2) / n))
+    if den == 0: return 0
+
+    r = num / den
+    return r
