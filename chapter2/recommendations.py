7fce3ee1113fe8dca4b5a0dbfdcfe51f09b7a2b4
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-04-29 13:29:34 +0900

    ユークリッド距離の実装

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -16,3 +16,21 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
  'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+
+from math import sqrt
+
+# person1とperson2の距離から類似性スコアを返す
+def sim_distance(prefs, person1, person2):
+    # 二人共評価しているアイテムのリストを取得
+    shared_items={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            shared_items[item] = 1
+
+    if len(shared_items)==0: return 0
+
+    # sum_of_squares = sum([pow(critics[person1][item]- critics[person2][item], 2) for item in critics[person1] if item in critics[person2]])
+    sum_of_squares = sum(pow(prefs[person1][item]-prefs[person2][item], 2) for item in shared_items)
+
+    return 1/(1+sum_of_squares)
