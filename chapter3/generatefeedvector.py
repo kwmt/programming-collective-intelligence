9fd3cc3c419a25376fa24fe952f7d02176ab8108
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-03 00:13:39 +0900

    形態要素解析を組み込んでブログに現れる単語の出現回数をカウント

--- chapter3/generatefeedvector.py
+++ chapter3/generatefeedvector.py
@@ -1,5 +1,6 @@
 import feedparser
 import re
+import splitter
 
 def getwordcounts(url):
     d = feedparser.parse(url)
@@ -9,8 +10,18 @@ def getwordcounts(url):
         if 'summary' in e: summary=e.summary
         else: summary=e.description
 
-        
-    return summary
+        # 単語のリストを抽出
+        words = getwords(e.title + ' ' + summary)
+        for word in words:
+            word_count.setdefault(word, 0)
+            word_count[word] +=  1
 
+    return d.feed.title, word_count
+
+def getwords(html):
+    txt = re.compile(r'<[->]+>').sub('', html)
+    return [word.lower() for word in splitter.split(txt) if word != '']
+
+# サンプル
 result = getwordcounts('https://note.com/koukichi_t/rss')
 print(result)
