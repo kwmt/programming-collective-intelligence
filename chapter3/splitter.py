f5213ae94246df6d739060d54c53ede947b0fabd
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-02 23:59:19 +0900

    形態要素解析API実装

---
+++ chapter3/splitter.py
@@ -0,0 +1,28 @@
+
+import os
+from urllib.request import urlopen
+import urllib.parse
+from bs4 import BeautifulSoup
+
+
+# 形態要素解析した結果をリストで返す
+
+# app_idは こちらのclient idを指定
+# https://e.developer.yahoo.co.jp/dashboard/app.detail/IQfwfD6ypEMtT44-
+# YAHOO_CLIENT_APP_ID=<app_id> python splitter.py という感じで実行する。
+# https://developer.yahoo.co.jp/webapi/jlp/ma/v1/parse.html
+
+appId = os.environ['YAHOO_CLIENT_APP_ID']
+pageUrl = "https://jlp.yahooapis.jp/MAService/V1/parse"
+
+def split(sentence, appid= appId, results= 'ma', filter='1|2|4|5|9|10'):
+    sentence=urllib.parse.quote_plus(sentence.encode('utf-8'))
+    query= f"{pageUrl}?appid={appid}&results={results}&uniq_filter={filter}&sentence={sentence}"
+    result = urlopen(query)
+    soup = BeautifulSoup(result)
+
+    try:
+        return [l.surface.string for l in soup.ma_result.word_list]
+    except: return[]
+        
+
