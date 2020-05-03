897b5996f89c880885cdf4e0d4c33756af0a0280
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-03 23:19:44 +0900

    デンドログラム描画

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,5 +1,6 @@
 import csv
 from math import sqrt
+from PIL import Image, ImageDraw, ImageFont
 
 def readfile(filename):
     # タイトル(単語)
@@ -86,8 +87,7 @@ def hcluster(rows, distance=pearson):
         newcluster = bicluster(mergevec, left=clust[lowestpair[0]],
                         right=clust[lowestpair[1]],
                         distance=closest,id=currentclustid)
-        print(newcluster)
-        # 元のセットではないクラスタのIDは負にする
+         # 元のセットではないクラスタのIDは負にする
         currentclustid = -1
         del clust[lowestpair[1]]
         del clust[lowestpair[0]]
@@ -108,8 +108,65 @@ def printclust(clust, labels=None, n=0):
     if clust.left != None: printclust(clust.left, labels=labels, n = n+1)
     if clust.right != None: printclust(clust.right, labels=labels, n = n+1)
 
+def getheight(clust):
+  # Is this an endpoint? Then the height is just 1
+  if clust.left==None and clust.right==None: return 1
+
+  # Otherwise the height is the same of the heights of
+  # each branch
+  return getheight(clust.left)+getheight(clust.right)
+
+def getdepth(clust):
+    if clust.left == None and clust.right == None: return 0
+    return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+
+def drawdendrogram(clust,labels,jpeg='clusters.jpg'):
+  # height and width
+  h=getheight(clust)*20
+  w=1200
+  depth=getdepth(clust)
+
+  # width is fixed, so scale distances accordingly
+  scaling=float(w-150)/depth
+
+  # Create a new image with a white background
+  img=Image.new('RGB',(w,h),(255,255,255))
+  draw=ImageDraw.Draw(img)
+
+  draw.line((0,h/2,10,h/2),fill=(255,0,0))    
+
+  # Draw the first node
+  drawnode(draw,clust,10,(h/2),scaling,labels)
+  img.save(jpeg,'JPEG')
+
+def drawnode(draw,clust,x,y,scaling,labels):
+  if clust.id<0:
+    h1=getheight(clust.left)*20
+    h2=getheight(clust.right)*20
+    top=y-(h1+h2)/2
+    bottom=y+(h1+h2)/2
+    # Line length
+    ll=clust.distance*scaling
+    # Vertical line from this cluster to children    
+    draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))    
+    
+    # Horizontal line to left item
+    draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))    
+
+    # Horizontal line to right item
+    draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))        
+
+    # Call the function to draw the left and right nodes    
+    drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
+    drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
+  else:   
+    # If this is an endpoint, draw the item label
+    fnt = ImageFont.truetype('./Kokoro.otf',30) #ImageFontインスタンスを作る
+    draw.text((x+5,y-7),labels[clust.id],(0,0,0), font=fnt)
+
 
 blognames, words, data= readfile('blogdata.csv')
 clust=hcluster(data)
 
-printclust(clust, labels=blognames)
+drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
