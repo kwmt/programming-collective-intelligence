4d2bcd761ed519673f86c948db8f4eb2121e77d4
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-03 12:12:38 +0900

    各単語の頻出度を調べる

--- chapter3/generatefeedvector.py
+++ chapter3/generatefeedvector.py
@@ -37,3 +37,39 @@ def fetch_urls_data():
 # サンプル
 # result = getwordcounts('https://note.com/koukichi_t/rss')
 # print(fetch_urls_data())
+
+
+# 各URLの単語を計測
+apcount = {}
+wordcounts={}
+feedlist = [line for line in open('feedlist.txt')]
+
+for feedurl in feedlist:
+    try:
+        title, wc = getwordcounts(feedurl)
+        wordcounts[title] = wc
+        for word, count in wc.items():
+            apcount.setdefault(word,0)
+            if count > 1:
+                apcount[word]+=1
+    except:
+        print(f"Failed to parse feed {feedurl}")
+
+# 単語の出現数リストを生成
+# 出現率が多すぎるもの、低すぎるものをフィルタリングする。（theのような単語はほとんどすべての存在するため）
+wordlist=[]
+for w, bc in apcount.items():
+    frac = float(bc) / len(feedlist)
+    if frac > 0.1 and frac < 0.5: wordlist.append(w)
+
+# ブログのリストを縦に、単語リストを横に表示し、各ブログの単語の出現数をあらわした表を作成
+out = open('blogdata.txt', 'w')
+out.write('Blog')
+for word in wordlist: out.write(f"\t{word}")
+
+for blog, wc in wordcounts.items():
+    out.write(blog)
+    for word in wordlist:
+        if word in wc: out.write(f"\t{wc[word]}")
+        else: out.write("\t0")
+    out.write("\n")
