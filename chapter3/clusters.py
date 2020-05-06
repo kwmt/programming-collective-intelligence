486889e0bf6dda3b484893805d6cd34659602c9b
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-06 23:24:07 +0900

    kクラスタリング

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,6 +1,7 @@
 import csv
 from math import sqrt
 from PIL import Image, ImageDraw, ImageFont
+import random
 
 def readfile(filename):
     # タイトル(単語)
@@ -165,8 +166,46 @@ def drawnode(draw,clust,x,y,scaling,labels):
     fnt = ImageFont.truetype('./Kokoro.otf',30) #ImageFontインスタンスを作る
     draw.text((x+5,y-7),labels[clust.id],(0,0,0), font=fnt)
 
+def kcluster(rows, distance=pearson, k=4):
+    # それぞれのポイントの最小値、最大値を決める
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
+    # 重心をランダムにk個配置する
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+    lastmatches = None
+
+    for t in range(100):
+        print(f"Iteration {t}")
+        bestmatches=[[] for i in range(k)]
+        # それぞれの行に対して、もっとも近い重心を探し出す
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d =  distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row): bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches == lastmatches: break
+        lastmatches = bestmatches
+
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+    
+    return bestmatches
+
+
+
 
 blognames, words, data= readfile('blogdata.csv')
-clust=hcluster(data)
+clust=kcluster(data)
+print(clust)
 
-drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+[print(blognames[r]) for r in clust[0]]
+# drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
