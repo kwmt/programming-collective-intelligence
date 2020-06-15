fd102643600f19a535d2f195a36a3e92302ba0b5
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-06-15 22:48:41 +0900

    シンプルなクローラについて

--- chapter4/searchengine.py
+++ chapter4/searchengine.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 class clawler:
     # データベースの名前でクローラを初期化
@@ -29,7 +32,38 @@ class clawler:
         pass
 
     def crawl(self, pages, depth=2):
-        pass
+        for i in range(depth):
+            newpages=set()
+            for page in pages:
+                try:
+                    c=requests.get(page)
+                except:
+                    print(f"Could not open {page}")
+                    continue
+                soup = BeautifulSoup(c.text, 'html.parser')
+                self.addtoindex(page, soup)
+
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page, link['href'])
+                        if (url.find("'") != -1): continue
+                        url=url.split('#')[0] # アンカーを取り除く
+                        if ((url[0:4] == 'http' or url[0:4] == 'https') and not self.isindexed(url)):
+                            newpages.add(url)
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page, url, linkText)
+            
+                self.dbcommit()
+            pages = newpages
+
+
 
     def createindextables(self):
         pass
+
+
+
+pagelist = ['https://kwmt27.net/']
+crawler = clawler('')
+crawler.crawl(pagelist)
