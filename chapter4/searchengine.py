2dd84e7b6f1da3319473eb1ff731b4c45b9bb48c
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-06-18 22:05:02 +0900

    インデックスの作成しインデックスの追加

--- chapter4/searchengine.py
+++ chapter4/searchengine.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import sqlite3
+import splitter
+
+# Create a list of words to ignore
+ignorewords={'the':1,'of':1,'to':1,'and':1,'a':1,'in':1,'is':1,'it':1}
 
 class clawler:
     # データベースの名前でクローラを初期化
@@ -15,22 +19,77 @@ class clawler:
         self.con.commit()
 
     def getentryid(self, table, field, value, createnew=True):
-        return None
+        cur = self.con.execute("select rowid from %s where %s='%s'" % (table, field, value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute("insert into %s (%s) values('%s')" % (table, field, value)) 
+            return cur.lastrowid
+        else:
+            return res[0]
 
     def addtoindex(self, url, soup):
+        if self.isindexed(url): return
         print(f"Indexing {url}")
 
+        # 個々の単語を取得する
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        # URL idを取得する
+        urlid = self.getentryid('urllist', 'url', url)
+
+        # それぞれの単語と、このurlのリンク
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute("insert into wordlocation(urlid, wordid, location)  \
+                values (%d, %d, %d)" % (urlid, wordid, i))
+
+
+
+    # ページ内の単語を探し出す
     def gettextonly(self, soup):
-        return None
+        v = soup.string
+        if v == None:
+            c = soup.contents
+            resulttext = ''
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
     
-    def separatewords(self, test):
-        return None
+    def separatewords(self, text):
+        # print(f"{text}")
+        try:
+            return [s.lower() for s in splitter.split(text) if s != '']
+        except:
+            print(f"separatewords{text}")
+            return []
     
     def isindexed(self, url):
-        return None
+        u = self.con.execute("select rowid from urllist where url = '%s'" % url).fetchone()
+        if u != None:
+            v = self.con.execute('select * from wordlocation where urlid=%d' % u[0]).fetchone()
+            if v != None: return True
+        else:
+            return False
     
     def addlinkref(self, urlFrom, urlTo, linkText):
-        pass
+        words = self.separatewords(linkText)
+        fromid = self.getentryid('urllist', 'url', urlFrom)
+        toid = self.getentryid('urllist', 'url', urlTo)
+        if fromid == toid: return
+
+        cur = self.con.execute("insert into link(fromid, toid) values (%d, %d)" % (fromid, toid))
+        linkid = cur.lastrowid
+        for word in words:
+            if word in ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute("insert into linkwords(linkid, wordid) values (%d, %d)" % (linkid, wordid)) 
+
 
     def crawl(self, pages, depth=2):
         for i in range(depth):
@@ -61,10 +120,21 @@ class clawler:
 
 
     def createindextables(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbcommit()
 
 
 
 pagelist = ['https://kwmt27.net/']
-crawler = clawler('')
+crawler = clawler('searchindex.db')
+# crawler.createindextables()
 crawler.crawl(pagelist)
